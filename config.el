c5e98c38782a986053a8caeb2a17bbbf5dd8cdb7
Ualnomis <ualnamnuhc@gmail.com>
2024-02-13 18:25:05 +0800

    style: remove unnecessary line

--- config.el
+++ config.el
@@ -79,7 +79,6 @@
   (add-hook 'eshell-before-prompt-hook
             (lambda ()
               (setq xterm-color-preserve-properties t)))
-
   (add-to-list 'eshell-preoutput-filter-functions 'xterm-color-filter)
   (setq eshell-output-filter-functions (remove 'eshell-handle-ansi-color eshell-output-filter-functions))
   (setenv "TERM" "xterm-256color"))
