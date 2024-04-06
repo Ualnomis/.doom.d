7084aa9cd3851e9ef34fdf4090042702c14c79d8
Ualnomis <ualnamnuhc@gmail.com>
2024-04-06 12:20:58 +0800

    feat: open with emacsclient

--- open-with-emacs.bat
+++ open-with-emacs.bat
@@ -6,6 +6,6 @@ set "winFilePath=%~1"
 for /f "tokens=*" %%i in ('wsl wslpath "%winFilePath%" ') do set "wslFilePath=%%i"
 
 :: Run the command in Pengwin
-"%LOCALAPPDATA%\Microsoft\WindowsApps\pengwinw.exe" run cd ~;env PENGWIN_COMMAND='/usr/bin/emacs \"%wslFilePath%\"' bash -l -c echo
+"%LOCALAPPDATA%\Microsoft\WindowsApps\pengwinw.exe" run cd ~;env PENGWIN_COMMAND='emacsclient -c -a \"emacs\" \"%wslFilePath%\"' bash -l -c echo
 
 endlocal
